899f9d1539673ef3515dd8050b4bea20dda1acda
Jonathan <jonathan@maximasoftware.co.za>
2018-02-10 16:44:19 +0200

    Suppliers

--- HappyFish/HappyFish/Nutrition.cs
+++ HappyFish/HappyFish/Nutrition.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HappyFish
+{
+    public class Nutrition
+    {
+        public string Name;
+        public decimal Value;
+        public decimal Frequency;
+        public string FrequencyMeasuringPeriod;
+
+    }
+}
