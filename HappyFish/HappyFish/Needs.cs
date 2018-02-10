79ef247c71e128a450c77ef76c7dbbe8abe742d4
Jonathan <jonathan@maximasoftware.co.za>
2018-02-10 16:32:42 +0200

    Needs - Adjusted

--- HappyFish/HappyFish/Needs.cs
+++ HappyFish/HappyFish/Needs.cs
@@ -8,8 +8,11 @@ namespace HappyFish
     public class Needs
     {
         public LifeStage LifeStage;
-        public Nutrition Nutrition;
-        public TankEnvironment MinEnvironment;
-        public TankEnvironment MaxEnvironment;
+        public List<Nutrition> Nutrition;
+        public TankEnvironment MinEnvironment;// PH 3.7 // Amonia 0.1mg p l 
+        public TankEnvironment MaxEnvironment;// PH 11 // Ammonia 7.1mg p l
+        public TankEnvironment MinRecommended;// PH 7 // Oxygen 7ppm // 
+        public TankEnvironment MaxRecommended;// PH 9 //Density 2 Pounds per Cubic Foot
+        public List<string> Preventions;
     }
 }
