8505e8f740985e6d7bedbbd6ea7982132fe22dc5
Jonathan <jonathan@maximasoftware.co.za>
2018-02-10 17:37:56 +0200

    Measuring Criterias

--- HappyFish/HappyFish/TankEnvironment.cs
+++ HappyFish/HappyFish/TankEnvironment.cs
@@ -7,10 +7,13 @@ namespace HappyFish
 {
     public class TankEnvironment
     {
+        public int TankEnvironmentId;
+
         public int TankId{ get; set; }
         public int FisheryId{ get; set; }
+        public int NeedId { get; set; }
 
         public DateTime EventTime{ get; set; }
-        public Dictionary<string, string>MeasuringItem { get; set; }
+        public List<MeasuringCriteria>MeasuringCriterias { get; set; } // Todo: Figure Out a Way Generically to Add a Generic List of Measurement Criteria
     }
 }
